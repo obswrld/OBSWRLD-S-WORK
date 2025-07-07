758c6c7f0e5ccf18b351304b018be2f1c8180f04
Republic Oba <republicoba1@gmail.com>
2025-07-07 01:14:01 +0100

    new works

--- PYTHON/MOVIE/src/movie/movie_rating.py
+++ PYTHON/MOVIE/src/movie/movie_rating.py
@@ -1,60 +1,48 @@
 import datetime
-from operator import index
-from tkinter.font import names
-
 
 class Movie:
+    def __init__(self, name):
+        self.name = name
+        self.rating = []
+        self.added_time = datetime.datetime.now()
+
+    def add_rating(self, rating):
+        if 1 <= rating <= 5:
+            self.rating.append(rating)
+            print(f"Rating added for {self.name} : {rating}")
+        else:
+            print("Ratting between 1 and 5.")
+
+    def  average_rating(self):
+        if self.rating:
+            return sum(self.rating) / len(self.rating)
+        return 0.0
+
+
+class MovieRatingSystem:
     def __init__(self):
-        self.movie_list =  []
+        self.movies = {}
 
-    def add_movies(self, movie):
-        current_movie = {
-                                "title": movie.lower(),
-                                "rating": [],
-                                "date": datetime.date.today()
-                                }
-        self.movie_list.append(current_movie)
+    def add_movie(self, name):
+        if name not in self.movies:
+            self.movies[name] = Movie(name)
+            print(f"Movie '{name}' added.")
+        else:
+            print(f"Movie '{name}' already exists.")
 
-    def get_movies(self):
-        return self.movie_list
+    def rate_movie(self, name, rating):
+        if name in self.movies:
+            self.movies[name].add_rating(rating)
+        else:
+            print(f"Movie '{name}' does not exist.")
 
-    def get_movie(self,  movie_name):
-        for each_movie in self.movie_list:
-            if each_movie["title"] == movie_name.lower():
-               return each_movie["title"]
-        return None
+    def view_average_rating(self):
+        if not self.movies:
+            print("No movies added.")
+            return
 
-def  set_rating(movie, rating, self):
-    if rating < 1 or rating > 5:
-        return "Invalid rating"
-    for current_movie in self.movie_list:
-        if current_movie["title"] == movie.lower():
-            current_movie["rating"].append(rating)
-    return None
-
-
-def get_movie(movie):
-    return movie.get_movie(movie.lower())
-
-
-def average_rating(movie):
-    current_movie = get_movie(movie)
-    ratings = current_movie["rating"]
-    if not ratings:
-        return "No ratings"
-    return sum(ratings) / len(ratings)
-
-def average_of_all_ratings(movie, self):
-    average = 0
-    for current_movie in self.movie_list:
-        one_rating = sum(current_movie["rating"])
-        average += one_rating / len(current_movie["rating"])
-        average /= len(current_movie["rating"])
-        average = round(average, 2)
-        return average
-    return None
-
-
-
-
+        print("Average ratings:")
+        for movie in self.movies.values():
+            avg = movie.average_rating()
+            print(f"{movie.name}: {avg: .2f}")
 
---
+++ PYTHON/MOVIE/src/movie/movie_rating_system.py
@@ -0,0 +1,11 @@
+from movie.movie_rating import Movie
+from movie.movie_rating import MovieRatingSystem
+
+def main():
+    system = MovieRatingSystem()
+
+
+    while True:
+        print("\n1. Add movie")
+        print("2. Rate movie")
+        
