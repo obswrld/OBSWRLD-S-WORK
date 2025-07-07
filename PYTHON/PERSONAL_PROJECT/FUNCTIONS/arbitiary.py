47d3265bf3cbee00a2571d5bcebe32d3b15fa9fc
Republic Oba <republicoba1@gmail.com>
2025-07-07 17:46:19 +0100

    new

---
+++ PYTHON/PERSONAL_PROJECT/FUNCTIONS/arbitiary.py
@@ -0,0 +1,9 @@
+def get_product(num1, num2, num3, num4, num5):
+    return num1 * num2 * num3 * num4 * num5
+
+number1 = 50
+number2 = 5
+number3 = 4
+number4 = 56
+number5 = 7
+print(get_product(number1, number2, number3, number4, number5))
