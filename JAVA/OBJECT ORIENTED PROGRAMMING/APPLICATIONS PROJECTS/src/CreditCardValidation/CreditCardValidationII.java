223e566bdccab97ebbf974e54842a558c5bca2e5
Republic Oba <republicoba1@gmail.com>
2025-07-10 15:41:33 +0100

    updated

--- JAVA/OBJECT ORIENTED PROGRAMMING/APPLICATIONS PROJECTS/src/CreditCardValidation/CreditCardValidationII.java
+++ JAVA/OBJECT ORIENTED PROGRAMMING/APPLICATIONS PROJECTS/src/CreditCardValidation/CreditCardValidationII.java
@@ -1,4 +1,25 @@
 package CreditCardValidation;
 
 public class CreditCardValidationII {
+
+    public static boolean validateCreditCardNumber(String cardNumber){
+        if(cardNumber.length() < 13 || cardNumber.length() > 16){
+            return false;
+        }
+        int totalSum = 0;
+        boolean isEven = false;
+        for(int i = cardNumber.length() - 1; i >= 0; i--){
+            int digit = Character.getNumericValue(cardNumber.charAt(i));
+
+            if(isEven){
+                digit *= 2;
+                if(digit > 9){
+                    digit -= 9;
+                }
+            }
+            totalSum += digit;
+            isEven = !isEven;
+        }
+        return (totalSum % 10 == 0);
+    }
 }
