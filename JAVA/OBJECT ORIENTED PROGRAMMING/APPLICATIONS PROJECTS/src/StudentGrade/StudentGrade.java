f10a4990b3761c6f1510af7e389e278e84f293fe
Republic Oba <republicoba1@gmail.com>
2025-07-10 14:27:27 +0100

    updated

--- JAVA/OBJECT ORIENTED PROGRAMMING/APPLICATIONS PROJECTS/src/StudentGrade/StudentGrade.java
+++ JAVA/OBJECT ORIENTED PROGRAMMING/APPLICATIONS PROJECTS/src/StudentGrade/StudentGrade.java
@@ -3,45 +3,27 @@ package StudentGrade;
 import java.util.Scanner;
 
 public class StudentGrade {
+    private static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter The number of students you have: ");
-        int students = input.nextInt();
+        int students = StudentGradeFunction.getNumberOfStudents();
+        int subjects = StudentGradeFunction.getNumberOfSubjects();
+        System.out.println("Saving.........");
+        System.out.println("Saved successfully");
 
-        System.out.print("Enter The number of subjects for the student you have: ");
-        int subjects = input.nextInt();
-        if(subjects < 9 || subjects > 16){
-            System.out.println("A Student must have a subject between 9 and 16");
-            System.out.print("Enter The number of subjects for the student you have: ");
-            subjects = input.nextInt();
-        } else{
-            System.out.print("Saving.........");
-            System.out.print("Saved successfully");
-        }
-
+        String[] studentNames = new String[students];
         int[][] scores = new int[students][subjects];
-        String[] studentName = new String[students];
-        String[] subjectName = new String[subjects];
 
         for(int i = 0; i < students; i++){
-            System.out.print("Enter Student Name: ");
-            studentName[i] = input.next();
-
+            System.out.println("Enter Student" + (i+1) + " Name:");
+            studentNames[i] = input.nextLine();
             for(int j = 0; j < subjects; j++){
-                System.out.println("Enter Subject Name: ");
-                subjectName[j] = input.next();
-                System.out.println("Enter Score: ");
-                scores[i][j] = input.nextInt();
-                if(scores[i][j] < 0 && scores[i][j] > 100){
-                    System.out.println("Please enter a Valid score:  ");
-                    scores[i][j] = input.nextInt();
-                } else {
-                    scores[i][j] = scores[i][j];
-                    System.out.println("Saving.........");
-                    System.out.print("Saved successfully");
-                }
-
+                scores[i][j] = StudentGradeFunction.getScoreForSubject(j + 1);
             }
+            System.out.println("Saving.........");
+            System.out.println("Saved successfully");
         }
+
+        StudentGradeFunction.displayClassSummary(studentNames, scores);
     }
 }
--- JAVA/OBJECT ORIENTED PROGRAMMING/APPLICATIONS PROJECTS/src/StudentGrade/StudentGradeFunction.java
+++ JAVA/OBJECT ORIENTED PROGRAMMING/APPLICATIONS PROJECTS/src/StudentGrade/StudentGradeFunction.java
@@ -1,23 +1,22 @@
 package StudentGrade;
+
 import java.util.Scanner;
 
-public class StudentGradeII {
+public class StudentGradeFunction {
+    private static Scanner input = new Scanner(System.in);
 
-
-    Scanner input = new Scanner(System.in);
-
-    private static int getNumberOfStudents(Scanner input){
-        System.out.print("How many Students Do you have?  ");
+    static int getNumberOfStudents(){
+        System.out.println("How many Students Do you have?  ");
         return input.nextInt();
     }
-    private static int getNumberOfSubjects(Scanner input){
-        System.out.print("How many Subjects Do they offer?  ");
+    static int getNumberOfSubjects(){
+        System.out.println("How many Subjects Do they offer?  ");
         return input.nextInt();
     }
-    private static int getScoreForSubject(int subject, Scanner input){
+    static int getScoreForSubject(int subjects){
         int score = 0;
         do {
-            System.out.print("Enter Score for Subject" + subject + ": ");
+            System.out.print("Enter Score for Subject" + subjects + ": ");
             score = input.nextInt();
             if(score < 0 || score > 100){
                 System.out.println("Invalid Input");
@@ -25,14 +24,14 @@ public class StudentGradeII {
         } while (score < 0 || score > 100);
         return score;
     }
-    private static void displayClassSummary(String[] studentNames, int[][] scores){
-        System.out.print("========================================================");
+    static void displayClassSummary(String[] studentNames, int[][] scores){
+        System.out.println("================================================================");
         System.out.printf("%-20s", "Student");
         for(int i = 0; i < scores[0].length; i++){
             System.out.printf("sub%d\t", (i + 1));
         }
         System.out.printf("%-10s%-10s%-10s%n", "Total", "Average", "Position");
-        System.out.println("=====================================================");
+        System.out.println("=================================================================");
 
         for(int i = 0; i < scores.length; i++){
             int total = 0;
@@ -40,14 +39,13 @@ public class StudentGradeII {
                 total += scores[i][j];
             }
             double average = (double)total / scores[i].length;
-            System.out.printf("%-20", studentNames[i]);
+            System.out.printf("%-20s", studentNames[i]);
             for(int j = 0; j < scores[i].length; j++){
                 System.out.printf("%d\t", scores[i][j]);
             }
-            System.out.printf("%-10d%-10.2f%n", total, average);
+            System.out.printf("%-10d%-10.2f%n",  total,   average);
         }
 
         System.out.println("=====================================================");
     }
-
 }
